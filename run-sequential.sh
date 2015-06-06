30a3dcd45cec073c3ec860c3ec4204d50d4678cc
krris <krrisgit@gmail.com>
2015-06-06 11:30:40 +0200

    Set N=100 and fix CMakeLists.txt.

--- run-sequential.sh
+++ run-sequential.sh
@@ -1,29 +1,8 @@
 #!/bin/sh
 
-delimiter="============================\n"
-
 generations=100
 initialPopulation=10000
 tempPopulation=20000
-title="Sequential version"
 
-echo ${title} " [N = 2]"
-build/PORR ${generations} ${initialPopulation} ${tempPopulation} 0.2 2 sequantial
-#echo ${delimiter}
-
-echo ${title} " [N = 10]"
-build/PORR ${generations} ${initialPopulation} ${tempPopulation} 0.2 10 sequential
-#echo ${delimiter}
-
-echo ${title} " [N = 20]"
-build/PORR ${generations} ${initialPopulation} ${tempPopulation} 0.2 20 sequential
-#echo ${delimiter}
-
-echo ${title} " [N = 50]"
-build/PORR ${generations} ${initialPopulation} ${tempPopulation} 0.2 50 sequential
-#echo ${delimiter}
-
-echo ${title} " [N = 100]"
 build/PORR ${generations} ${initialPopulation} ${tempPopulation} 0.2 100 sequential
-#echo ${delimiter}
 
